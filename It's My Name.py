69a4706d314a6b602de6ae08c70f63a7859633e5
Nithsihkumar003 <144939857+Nithsihkumar003@users.noreply.github.com>
2023-10-21 16:42:14 +0530

    Add files via upload

--- It's My Name.py
+++ It's My Name.py
@@ -42,16 +42,19 @@ for row in range(length):
             print('*',end=' ')
         else:
             print(' ',end=' ')
-    print()
+    print(     )            
+    
 
 #FOR S     
-for row in range(length):
-    for column in range(length):
-        if row == 0 or row == 4 or row == 2: 
+ 
+   
+for row in range(5):
+    for column in range(5):
+        if (row == 0 or row == 2 or row == 4) or column == 0 and (row > 0 and row < 2) or column == 4 and (row > 2 and row < 4):  
             print('*',end=' ')
         else:
             print(' ',end=' ')
-    print()
+    print(   )
  
 #FOR H    
 for row in range(length):
@@ -60,7 +63,59 @@ for row in range(length):
             print('*',end=' ')
         else:
             print(' ',end=' ')
-    print()
+    print(  )
+
+#For  K
+
+for row in range(5):
+    for column in range(5):
+        if column == 0 or column == 1 and (row > 1 and row < 3)or column == 2 and (row > 0 and row < 2)or row == 0  and (column > 2)or row == 3 and (column > 1 and column < 3)or row == 4  and (column > 2):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print(  )
+
+#For U
+for row in range(5):
+    for column in range(5):
+        if column == 0 or column == 4 or row == 4:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print(  )
+
+#for M 
+
+for row in range(5):
+    for column in range(5):
+        if column == 0 or column == 4 or (row == 1 and (column > 0 and column < 2 ) or row == 1 and column > 2 and column < 4 ) or row == 2 and column > 1 and column < 3 :
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print(  )   
+
+#For A
+for row in range(5):
+    for column in range(5):
+        if column == 0 or column == 4 or row == 0 or row == 2:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print(         )  
+
+#For R
+for row in range(5):
+    for column in range(5):
+        if column == 0 or row == 0 or column == 4 and (row > 0 and row < 3) or row == 2 and (column > 1 ) or row == 3 and (column > 2 and column < 4) or row == 4 and (column > 3):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print(            )  
+  
+
+  
+
+
 
 
 
